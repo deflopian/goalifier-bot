38c852e38ae045cfa49f1fd5ec985622da5e8a2d
Vadim Bannov <vadim@massluminosity.com>
2023-02-25 21:47:17 +0400

    debug keyboard

--- src/classes/tg-bot-wrapper.ts
+++ src/classes/tg-bot-wrapper.ts
@@ -177,6 +177,7 @@ export class TelegramBotWrapper {
     this.botContext?.setContext(chatId, TGContextTypes.WAITING_FOR_ACHIEVEMENT_GOAL_SELECTION)
 
     const keyboard = this.drawGoalKeyboard(user._id, 1)
+    console.log(keyboard)
     const options = Object.assign({ parse_mode: 'HTML' as ParseMode }, keyboard)
     this.sendL10NMessage(msg, L10nTypes.ACHIEVEMENTS_CREATION_WAITING_FOR_GOAL, options)
   }
