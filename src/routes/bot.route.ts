62de838d443232dcddf77ccd84daf2133fe309a9
Vadim Bannov <vadim@massluminosity.com>
2023-02-25 12:11:40 +0400

    bot test

--- src/routes/bot.route.ts
+++ src/routes/bot.route.ts
@@ -4,6 +4,10 @@ import { bot } from '../bot'
 const router = Router()
 const token = process.env.TG_TOKEN || ''
 
+router.get('/test', (req: Request, res: Response) => {
+  res.json('success')
+})
+
 router.post(`/bot${token}`, (req: Request, res: Response) => {
   bot.processUpdate(req.body)
   res.sendStatus(200)
